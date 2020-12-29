9936468a53fbbbee7853fc896c4c43ee7bf8edae
clobber <brymaster@gmail.com>
2020-12-28 23:59:45 -0700

    Update GLideN64 submodule reference.

---
+++ Compatibility/GLideN64.cpp
@@ -0,0 +1,7 @@
+//#include "Revision.h"
+char pluginName[] = "GLideN64";
+// uhh, what?
+//char pluginNameWithRevision[] = "GLideN64 rev." PLUGIN_REVISION;
+char pluginNameWithRevision[] = "GLideN64";
+wchar_t pluginNameW[] = L"GLideN64";
+void (*CheckInterrupts)( void );
